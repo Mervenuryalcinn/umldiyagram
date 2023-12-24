5c5be2e73a5a61fd15c537f132efc6110766081b
mervenuryalçın <127102075+Mervenuryalcinn@users.noreply.github.com>
2023-12-24 19:56:41 +0300

    Update Kiraci.java

--- diyagram/src/Kiraci.java
+++ diyagram/src/Kiraci.java
@@ -2,14 +2,17 @@ public class Kiraci{
     evSahibi evSahibi;
     String name;
     String phoneNumber;
+    // Kiraci sınıfının kurucu metodu
     public Kiraci(String name,String phoneNumber,evSahibi evSahibi){
         this.name=name;
         this.evSahibi=evSahibi;
         this.phoneNumber=phoneNumber;
     }
     public void displayInfo(){
-        // public evSahibi owner;//Bilgileri görüntüleme demektir.
+      //Bilgileri görüntüleme demektir.
+         // Kiracının adı ve telefon numarası görüntülenir
         System.out.println("\nKiracının adı: " + name + "\ntelefonbilgisi: "+phoneNumber+"\n\nEv sahibinin bilgileri:");
+          // Kiracının ev sahibinin bilgilerini görüntülemek için evSahibi sınıfının displayInfo metodu çağrılır
         evSahibi.displayInfo();
     }
 }
